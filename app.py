815949228962265f54627d30c382509764e19eb5
Brandon Hills <brandon_w_hills@college.harvard.edu>
2018-01-19 02:44:05 +0200

    Merging branches

--- app.py
+++ app.py
@@ -51,8 +51,10 @@ def loading():
 @app.route('/result', methods=['GET'])
 def result(path="static/images/uploads/"):
   # get photo from /static/images/results
-  sketch_img_path = path+os.listdir(path)[0]
-  result_img_path = path+os.listdir(path)[1]
+  dir_path = os.listdir(path)
+  dir_path.sort()
+  sketch_img_path = path+dir_path[0]
+  result_img_path = path+dir_path[1]
   return render_template('result.html', sketch_img_path=sketch_img_path, result_img_path=result_img_path)
 
 @app.route('/databases', methods=['GET'])
@@ -65,7 +67,13 @@ def knn():
   return render_template('knn.html', nn_paths=nn_paths)
 
 def knn_model():
-  upload_path = "static/images/uploads/combinas_fake_B.png"
+
+  path = "static/images/uploads/"
+  dir_path = os.listdir(path)
+  dir_path.sort()
+  result_img_path = path+dir_path[1]
+
+  upload_path = result_img_path #"static/images/uploads/combinas_fake_B.png"
   image = face_recognition.load_image_file(upload_path)
 
   with open('pickle/encodings.pkl', 'rb') as fp:
@@ -75,7 +83,7 @@ def knn_model():
       names = pickle.load(fp)
 
 
-  upload_path = "static/images/uploads/combinas_fake_B.png"
+  upload_path = result_img_path # "static/images/uploads/combinas_fake_B.png"
   image = face_recognition.load_image_file(upload_path)
   face_to_compare = face_recognition.face_encodings(image)[0]
 
@@ -103,13 +111,27 @@ def pix2pix(filename):
 
   ### 2. Concatenate Images
   img = cv2.cvtColor(cv2.imread(upload_path+filename), cv2.COLOR_BGR2RGB)
-  img = cv2.resize(img, (200, 250))
   img_combinas = np.concatenate([img, img], 1)
   cv2.imwrite(test_path+"combinas.png", img_combinas)
 
   ### 3. Perform Image Translation
   os.system("python pix2pix/test.py --dataroot pix2pix/datasets/faces --name faces_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction BtoA --dataset_mode aligned --norm batch --gpu_id -1")
-  os.system("mv " + save_dir+"images/combinas_fake_B.png " + upload_path+"combinas_fake_B.png")
+  
+
+  i = Image.open(upload_path+filename)
+  image = Image.open(save_dir+"images/combinas_fake_B.png")
+  image = image.resize(i.size,Image.ANTIALIAS)
+  os.system("rm " + save_dir+ "images/combinas_fake_B.png")
+  image = np.array(image)
+
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  cv2.imwrite(upload_path+filename[:-4]+"_photo.png", image)
+  
+
+  
+
+
+  # os.system("mv " + save_dir+"images/combinas_fake_B.png " + upload_path+"combinas_fake_B.png")
 
 
 # @app.route('/login')
